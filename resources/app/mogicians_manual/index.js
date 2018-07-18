91c1c9f77b88c98f9886f5fc746c6aa77d1aec23
Xmader <xmader@outlook.com>
2018-07-18 20:13:58 +0800

    膜法指南响应式设计模式

--- resources/app/mogicians_manual/index.js
+++ resources/app/mogicians_manual/index.js
@@ -11,13 +11,13 @@ const getArgs = () => {
 
 const init_modal = (key, a) => {
     $("#m_title").text(json[key]["titles"][a])
-    $("#m_body").html("<p>"+json[key]["contents"][a].replace(/\n/g, "</p><p>"))
+    $("#m_body").html("<p>" + json[key]["contents"][a].replace(/\n/g, "</p><p>"))
     $("#m_unformatted_body")[0].value = json[key]["contents"][a]
 }
 
 const init_video_img_modal = (src, title, type) => {
     $("#m_title").text(title)
-    $("#m_body").html(type == "img" ? `<img src="${src}" />` : `<video src="${src}" preload="Metadata" controls></video>`)
+    $("#m_body").html(type == "img" ? `<img src="${src}" class="modal_media" />` : `<video src="${src}" class="modal_media" preload="Metadata" controls></video>`)
     $("#copy").hide()
 
     $(".download_video").remove()
@@ -26,7 +26,7 @@ const init_video_img_modal = (src, title, type) => {
 
 var is_electron_app = navigator.userAgent.indexOf("Electron") > -1
 const is_Firefox = navigator.userAgent.indexOf("Firefox") > -1;
-const is_Chrome = navigator.userAgent.indexOf("Chrome") > -1;
+const is_Chrome = (navigator.userAgent.indexOf("Chrome") > -1) && window.chrome;
 
 var t = getArgs()["type"] || "shuo"
 
@@ -61,7 +61,7 @@ $.get("https://mohu.oss-cn-shanghai.aliyuncs.com/" + t + ".json", (data) => {
                     break;
                 }
                 case "chang": {
-                    item_html += `<li class="list-group-item grey">${data[key][items[a]]}<a href="${data["url"]}${items[a]}.mp3" target="_blank" class="download_music" download><i class="fa fa-download" aria-hidden="true"></i></a><audio class="audio${is_Firefox ? "_Firefox" : ""}" src="${data["url"]}${items[a]}.mp3" controls></audio></li>`
+                    item_html += `<li class="list-group-item grey chang ${is_Chrome || is_electron_app ? "chang_chrome" : " "}">${data[key][items[a]]}<a href="${data["url"]}${items[a]}.mp3" target="_blank" class="download_music" download><i class="fa fa-download" aria-hidden="true"></i></a><audio class="audio${is_Firefox ? "_Firefox" : ""}" src="${data["url"]}${items[a]}.mp3" controls></audio></li>`
                     break;
                 }
                 case "videos": {
