b9be875f597fad61981a3075011a074a2b37f93c
Xmader <xmader@outlook.com>
2018-10-14 17:35:30 +0800

    优化

--- app/check_update.js
+++ app/check_update.js
@@ -1,77 +1,58 @@
-const process = require("process");
-const get_json = require("./get_json")
-const path = require('path');
-const { dialog, shell, app } = require('electron')
+/**
+ * 检查更新
+ * @author Xmader
+ * @copyright Copyright (c) 2018 Xmader
+ */
 
+const fetch = require("./libs/node-fetch")
+const path = require("path")
+const { dialog, shell, app } = require("electron").remote
 
-var _platform = process.platform == "win32" ? "windows" : process.platform;
-var platform = process.platform;
-var arch = process.arch
-arch = (arch == "arm") ? "armv7l" : arch
-arch = (arch == "x32") ? "ia32" : arch
+const versionCheckApi = "https://raw.githubusercontent.com/Xmader/mohu/master/app/package.json"
+const DownloadUrl = "https://github.com/Xmader/mohu/releases/latest"
 
-var version = app.getVersion()/*当前版本号, 在package.json中定义*/
-// var versionCheckApi = "https://raw.githubusercontent.com/Xmader/mohu/master/resources/app/package.json"/*检查更新Api地址*/
-var versionCheckApi = "https://mohu.oss-cn-shanghai.aliyuncs.com/package.json"
-var DownloadUrl = `https://mohu.oss-cn-shanghai.aliyuncs.com/MohuAPP-${platform}-${arch}.zip`
-var version_formatted = format_version(version)
-
-var find_new_version = (new_version) => {
+/**
+ * @param {string} version
+ * @param {string} new_version
+ */
+const found_new_version = (version, new_version) => {
     dialog.showMessageBox({
         type: "question",
         buttons: ["下载", "取消"],
         defaultId: 0,
         cancelId: 1,
-        title: `发现新版本!`,
+        title: "发现新版本!",
         message: "是否要下载新版本?",
         detail: `当前版本: v${version} ,\n新版本: v${new_version} `,
-        icon: path.join(__dirname, './logo.png'),
-    }, (response, checkboxChecked) => {
+        icon: path.join(__dirname, "assets/logo.png"),
+    }, (response) => {
         if (response == 0) {
             shell.openExternal(DownloadUrl)
         }
     })
 }
 
-var download_remote_version = function () {
-    const url = versionCheckApi
+const check_update = async () => {
+    const req = await fetch(versionCheckApi)
+    const data = await req.json()
 
-    get_json(url, (data) => {
-        var new_version = data["version"] // get_new_version
-        var new_version_formatted = format_version(new_version)
-        if (version_formatted < new_version_formatted) {
-            find_new_version(new_version)
-        }
-    })
+    const version = app.getVersion() // 当前版本号, 在package.json中定义
+    const version_formatted = format_version(version)
+
+    const new_version = data["version"]
+    const new_version_formatted = format_version(new_version)
+
+    if (version_formatted < new_version_formatted) {
+        found_new_version(version, new_version)
+    }
 }
 
-var manual_check_update = () => {
-    get_json(versionCheckApi, (data) => {
-        var new_version = data["version"] // get_new_version
-        var new_version_formatted = format_version(new_version)
-        if (version_formatted < new_version_formatted) {
-            find_new_version(new_version)
-        }
-        else {
-            dialog.showMessageBox({
-                type: "warning",
-                buttons: ["确定"],
-                defaultId: 0,
-                cancelId: 1,
-                title: `没有更新的版本`,
-                message: `当前版本 v${version} 是最新版本!`,
-                // icon: path.join(__dirname, './logo.png'),
-            })
-        }
-    })
+/**
+ * @param {string} e - 需要被格式化的版本号字符串
+ */
+const format_version = (e) => {
+    return e.split(".").map((e) => +e)
 }
 
-function format_version(e) {
-    return e.split(".").map(function (e) { return e - 0 })
-}
-
-module.exports = {
-    check_update: download_remote_version,
-    manual_check_update: manual_check_update
-}
+module.exports = check_update
 
--- app/packager.sh
+++ app/packager.sh
@@ -1,4 +1,4 @@
-platform=$1 # 操作系统名称: win32 或 linux
+platform=$1 # 操作系统名称: win32 或 linux 或 darwin
 if [ $2 ]
 then
    arch=$2 # 系统构架: x64 或 ia32 或 arm64 或 armv7l
@@ -6,22 +6,26 @@ else
    arch=x64
 fi
 
-if [ ${platform} = "darwin" -a $(uname -o) = "Msys" ]
-then
-    echo "由于系统限制，目前只能在*unix系统上打包darwin(MacOS)版的APP"
-    exit
-fi
+electron-packager . MohuAPP --app-copyright="Copyright (c) 2018 Xmader" \
+    --platform=${platform} --arch=${arch} --electron-version 2.0.4 \
+    --out ../OutApp/ --overwrite \
+    --icon=./assets/logo.ico --ignore=packager.sh \
+    --tmpdir=../OutApp/Temp/ --download.cache=../OutApp/Temp/ --download.mirror="https://npm.taobao.org/mirrors/electron/"
 
-electron-packager . MohuAPP --platform=${platform} --arch=${arch}  --out ../../OutApp/ --electron-version 2.0.4 --overwrite --icon=./logo.ico --tmpdir=../../OutApp/Temp/ --ignore=packager.sh --download.cache=../../OutApp/Temp/ --download.mirror="https://npm.taobao.org/mirrors/electron/"
-cd ../../OutApp/MohuAPP-${platform}-${arch}
+cd ../OutApp/MohuAPP-${platform}-${arch}
 rm LICENSE LICENSES.chromium.html version
 cp ../../README.md ./
+cp ../../LICENSE ./
 
 cd ../ # 在OutApp文件夹
-electron-installer-zip MohuAPP-${platform}-${arch} ../dist/MohuAPP-${platform}-${arch}.zip
-rm -rf MohuAPP-${platform}-${arch}
+cd "MohuAPP-${platform}-${arch}"
 
-python ../upload/main.py ../dist/MohuAPP-${platform}-${arch}.zip
-python ../upload/main.py ../resources/app/package.json
-
-rm ../dist/MohuAPP-${platform}-${arch}.zip
+if [ ${platform} = "darwin" ]
+then
+    electron-installer-zip MohuAPP.app ../../dist/MohuAPP-${platform}-${arch}.zip
+elif [ ${platform} = "linux" ]
+then
+    electron-installer-zip MohuAPP-${platform}-${arch} ../../dist/MohuAPP-${platform}-${arch}.zip
+else
+    electron-installer-zip . ../../dist/MohuAPP-${platform}-${arch}.zip
+fi
